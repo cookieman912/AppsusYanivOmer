c6878c8c668772d9f65efb76ff86f66d5ff1bbbd
Yaniv bar <yanivb527@gmail.com>
2021-06-23 18:46:54 +0300

    fixing commit 3

--- js/apps/email/cmps/email-preview.js
+++ js/apps/email/cmps/email-preview.js
@@ -1,19 +1,21 @@
 export default {
     props: ['email'],
     template: `
-            <li class="email-preview">     
-                <img v-if="email.isRead" src="img/read.png">
-                <img v-else src="img/unread.png">
-                <div class="preview-text">
-                    <div>{{email.userName}}</div>
-                    <router-link :to="'/mail/' + email.id">
-                        <div class="subject">{{email.subject}}</div>
-                    </router-link>
-                        <div><span>Sent:</span> {{email.sentAt}}</div>
-                    </div> 
-             </li>
+        <li class="email-preview">
+
+            <img v-if="email.isRead" src="img/read.png">
+
+            <img v-else src="img/unread.png">
+
+            <div>{{email.from}}</div>
+
+            <div class="subject">{{email.subject}}</div>
+
+            <div><span>Sent:</span> {{email.sentAt}}</div>
+            
+        </li>
     `,
     created() {
-        this.email.userName = this.email.from.substr(0, this.email.from.indexOf('@'));
+        console.log(this.email);
     }
 }
